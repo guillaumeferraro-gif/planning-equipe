e0363fecbf00b2198b1589b971baae2fa07c0bb2
guillaumeferraro-gif <guillaume.ferraro@gmail.com>
2026-06-02 21:55:22 +0200

    Update solver_logic.py

--- solver_logic.py
+++ solver_logic.py
@@ -2,7 +2,7 @@ from ortools.sat.python import cp_model
 import datetime
 import pandas as pd
 
-def generate_schedule(nurses, year, month, history, holidays):
+def generate_schedule(nurses, year, month, history, public_holidays):
     model = cp_model.CpModel()
     
     if month == 12:
@@ -13,10 +13,11 @@ def generate_schedule(nurses, year, month, history, holidays):
     tiih_days = []
     for d in range(1, num_days + 1):
         date_obj = datetime.date(year, month, d)
-        if date_obj.weekday() in [1, 3, 4] and date_obj not in holidays:
+        # Mardi(1), Jeudi(3), Vendredi(4). Pas de TIIH si férié.
+        if date_obj.weekday() in [1, 3, 4] and date_obj not in public_holidays:
             tiih_days.append(d)
 
-    # Variables : s=0: JOUR, s=1: NUIT, s=2: TIIH
+    # Variables : s0=JOUR, s1=NUIT, s2=TIIH
     shifts = {}
     for n in nurses:
         for d in range(1, num_days + 1):
@@ -29,8 +30,7 @@ def generate_schedule(nurses, year, month, history, holidays):
         if d in tiih_days:
             model.Add(sum(shifts[(n, d, 2)] for n in nurses) == 1) 
         else:
-            for n in nurses:
-                model.Add(shifts[(n, d, 2)] == 0)
+            for n in nurses: model.Add(shifts[(n, d, 2)] == 0)
 
     for n in nurses:
         for d in range(1, num_days + 1):
@@ -49,42 +49,39 @@ def generate_schedule(nurses, year, month, history, holidays):
                 window.append(h)
             model.Add(sum(window) <= 2880)
 
-    # Logique de blocs (Pénalités pour changements)
+    # Blocs et équité
     penalties = []
     for n in nurses:
         for d in range(1, num_days):
             for s in range(3):
-                change = model.NewBoolVar(f'change_{n}_{d}_{s}')
+                change = model.NewBoolVar(f'c_{n}_{d}_{s}')
                 model.Add(shifts[(n, d, s)] != shifts[(n, d+1, s)]).OnlyEnforceIf(change)
                 penalties.append(change)
 
-    # Équité
-    nurse_minutes = []
+    nurse_min = []
     for n in nurses:
-        total_m = sum(shifts[(n, d, 0)] * 720 + shifts[(n, d, 1)] * 720 + shifts[(n, d, 2)] * 462 for d in range(1, num_days + 1))
-        hist_m = int(history.get(n, 0) * 60)
-        nurse_minutes.append(total_m + hist_m)
+        total = sum(shifts[(n, d, 0)] * 720 + shifts[(n, d, 1)] * 720 + shifts[(n, d, 2)] * 462 for d in range(1, num_days + 1))
+        nurse_min.append(total + int(history.get(n, 0) * 60))
 
-    min_h = model.NewIntVar(0, 10000000, 'min_h')
-    max_h = model.NewIntVar(0, 10000000, 'max_h')
-    model.AddMinEquality(min_h, nurse_minutes)
-    model.AddMaxEquality(max_h, nurse_minutes)
+    min_h = model.NewIntVar(0, 10000000, '')
+    max_h = model.NewIntVar(0, 10000000, '')
+    model.AddMinEquality(min_h, nurse_min)
+    model.AddMaxEquality(max_h, nurse_min)
 
-    # OBJECTIF : Priorité à l'équité (coeff 10) puis aux blocs (coeff 1)
     model.Minimize(10 * (max_h - min_h) + sum(penalties))
 
     solver = cp_model.CpSolver()
-    solver.parameters.max_time_in_seconds = 5.0
+    solver.parameters.max_time_in_seconds = 8.0
     status = solver.Solve(model)
 
-    if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
-        data = []
+    if status in [cp_model.OPTIMAL, cp_model.FEASIBLE]:
+        # On retourne un dictionnaire {infirmière: {jour: poste}}
+        res = {n: {} for n in nurses}
         for d in range(1, num_days + 1):
-            row = {"Date": f"{d}/{month}", "Jour": "", "Nuit": "", "TIIH": ""}
             for n in nurses:
-                if solver.Value(shifts[(n, d, 0)]): row["Jour"] = n
-                if solver.Value(shifts[(n, d, 1)]): row["Nuit"] = n
-                if solver.Value(shifts[(n, d, 2)]): row["TIIH"] = n
-            data.append(row)
-        return pd.DataFrame(data)
+                if solver.Value(shifts[(n, d, 0)]): res[n][d] = "☀️ JOUR"
+                elif solver.Value(shifts[(n, d, 1)]): res[n][d] = "🌙 NUIT"
+                elif solver.Value(shifts[(n, d, 2)]): res[n][d] = "🛠️ TIIH"
+                else: res[n][d] = "-"
+        return res
     return None
