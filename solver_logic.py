e70e829b370bc8fb7de83138a37425e0e1eddbdc
guillaumeferraro-gif <guillaume.ferraro@gmail.com>
2026-06-03 11:22:51 +0200

    Update solver_logic.py

--- solver_logic.py
+++ solver_logic.py
@@ -10,20 +10,25 @@ def generate_schedule(nurses, year, month, history, public_holidays):
     else:
         num_days = (datetime.date(year, month + 1, 1) - datetime.date(year, month, 1)).days
     
-    tiih_days = []
-    for d in range(1, num_days + 1):
-        date_obj = datetime.date(year, month, d)
-        # Mardi(1), Jeudi(3), Vendredi(4). Pas de TIIH si férié.
-        if date_obj.weekday() in [1, 3, 4] and date_obj not in public_holidays:
-            tiih_days.append(d)
+    tiih_days = [d for d in range(1, num_days + 1) 
+                 if datetime.date(year, month, d).weekday() in [1, 3, 4] 
+                 and datetime.date(year, month, d) not in public_holidays]
 
     # Variables : s0=JOUR, s1=NUIT, s2=TIIH
     shifts = {}
+    # Variable de Phase : 1 si l'infirmière est en "Phase Nuit" pour la semaine W
+    # On divise le mois en 4 semaines pour plus de souplesse
+    phase_nuit = {} 
+
     for n in nurses:
+        for w in range(5): # Jusqu'à 5 semaines par mois
+            phase_nuit[(n, w)] = model.NewBoolVar(f'phaseNuit_n{n}_w{w}')
+        
         for d in range(1, num_days + 1):
             for s in range(3):
                 shifts[(n, d, s)] = model.NewBoolVar(f'n{n}_d{d}_s{s}')
 
+    # --- CONTRAINTES DE PRÉSENCE ---
     for d in range(1, num_days + 1):
         model.Add(sum(shifts[(n, d, 0)] for n in nurses) == 1) 
         model.Add(sum(shifts[(n, d, 1)] for n in nurses) == 1) 
@@ -32,50 +37,55 @@ def generate_schedule(nurses, year, month, history, public_holidays):
         else:
             for n in nurses: model.Add(shifts[(n, d, 2)] == 0)
 
+    # --- LOGIQUE DE PHASE ET BLOCS ---
     for n in nurses:
         for d in range(1, num_days + 1):
-            model.Add(sum(shifts[(n, d, s)] for s in range(3)) <= 1)
-            if d < num_days:
-                model.Add(shifts[(n, d, 1)] + shifts[(n, d+1, 0)] <= 1)
-                model.Add(shifts[(n, d, 1)] + shifts[(n, d+1, 2)] <= 1)
+            week = (d - 1) // 7
+            # Si on est en phase nuit cette semaine :
+            # 1. Interdiction de faire JOUR (s0)
+            model.Add(shifts[(n, d, 0)] == 0).OnlyEnforceIf(phase_nuit[(n, week)])
+            # 2. Interdiction de faire TIIH (s2)
+            model.Add(shifts[(n, d, 2)] == 0).OnlyEnforceIf(phase_nuit[(n, week)])
+            
+            # Si on est en phase JOUR (non phase nuit) :
+            # 1. Interdiction de faire NUIT (s1)
+            model.Add(shifts[(n, d, 1)] == 0).OnlyEnforceIf(phase_nuit[(n, week)].Not())
 
-    # 48h glissantes
+            # Sécurité standard
+            model.Add(sum(shifts[(n, d, s)] for s in range(3)) <= 1)
+
+        # --- MAX 3 SHIFTS D'AFFILÉE ---
+        for d in range(1, num_days - 2):
+            model.Add(sum(sum(shifts[(n, di, s)] for s in range(3)) for di in range(d, d + 4)) <= 3)
+
+        # --- WEEK-ENDS GROUPÉS ---
+        for d in range(1, num_days):
+            if datetime.date(year, month, d).weekday() == 5: # Samedi
+                model.Add(shifts[(n, d, 0)] == shifts[(n, d+1, 0)])
+                model.Add(shifts[(n, d, 1)] == shifts[(n, d+1, 1)])
+
+    # --- 48H GLISSANTES ---
     for n in nurses:
         for d in range(1, num_days - 5):
-            window = []
-            for i in range(7):
-                day = d + i
-                h = shifts[(n, day, 0)] * 720 + shifts[(n, day, 1)] * 720 + shifts[(n, day, 2)] * 462
-                window.append(h)
+            window = [shifts[(n, d+i, 0)] * 720 + shifts[(n, d+i, 1)] * 720 + shifts[(n, d+i, 2)] * 462 for i in range(7)]
             model.Add(sum(window) <= 2880)
 
-    # Blocs et équité
-    penalties = []
-    for n in nurses:
-        for d in range(1, num_days):
-            for s in range(3):
-                change = model.NewBoolVar(f'c_{n}_{d}_{s}')
-                model.Add(shifts[(n, d, s)] != shifts[(n, d+1, s)]).OnlyEnforceIf(change)
-                penalties.append(change)
-
+    # --- ÉQUITÉ ---
     nurse_min = []
     for n in nurses:
         total = sum(shifts[(n, d, 0)] * 720 + shifts[(n, d, 1)] * 720 + shifts[(n, d, 2)] * 462 for d in range(1, num_days + 1))
         nurse_min.append(total + int(history.get(n, 0) * 60))
 
-    min_h = model.NewIntVar(0, 10000000, '')
-    max_h = model.NewIntVar(0, 10000000, '')
+    min_h, max_h = model.NewIntVar(0, 10000000, ''), model.NewIntVar(0, 10000000, '')
     model.AddMinEquality(min_h, nurse_min)
     model.AddMaxEquality(max_h, nurse_min)
-
-    model.Minimize(10 * (max_h - min_h) + sum(penalties))
+    model.Minimize(max_h - min_h)
 
     solver = cp_model.CpSolver()
-    solver.parameters.max_time_in_seconds = 8.0
+    solver.parameters.max_time_in_seconds = 15.0
     status = solver.Solve(model)
 
     if status in [cp_model.OPTIMAL, cp_model.FEASIBLE]:
-        # On retourne un dictionnaire {infirmière: {jour: poste}}
         res = {n: {} for n in nurses}
         for d in range(1, num_days + 1):
             for n in nurses:
